61fa0bd410d48a2f5ff610f1c45b3cf621aefbb7
kanchirajput26 <78139026+kanchirajput26@users.noreply.github.com>
2026-03-09 13:59:33 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kanchirajput/",
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/'
 }));
